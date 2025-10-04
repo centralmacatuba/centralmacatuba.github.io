053414a49d388d6f85ff0f20d8729b06972c2850
Central Macatuba <centralmacatuba@gmail.com>
2025-10-04 15:35:49 -0300

    Add files via upload

---
+++ assets/footer.php
@@ -0,0 +1,71 @@
+    <div class="footer-container">
+        <!-- Coluna Central Macatuba -->
+        <div class="footer-col">
+            <h3>Central Macatuba</h3>
+            <p>© 2025 Central Macatuba. Todos os direitos reservados.</p>
+            <p>Email: <a href="mailto:contato@macatuba.eliasempresas.com">contato@macatuba.eliasempresas.com</a></p>
+            <p>Site: <a href="https://macatuba.eliasempresas.com" target="_blank">macatuba.eliasempresas.com</a></p>
+<div class="social-media">
+                <a href="https://youtube.com/@centralmacatuba" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/youtube.svg" class="social-icon"> YouTube
+                </a>
+                <a href="https://facebook.com/centralmacatuba" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/facebook.svg" class="social-icon"> Facebook
+                </a>
+                <a href="https://instagram.com/centralmacatuba" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/instagram.svg" class="social-icon"> Instagram
+                </a>
+                <a href="https://x.com/centralmacatuba" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/x-twitter.svg" class="social-icon"> X
+                </a>
+                <a href="https://tiktok.com/@centralmacatuba" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/tiktok.svg" class="social-icon"> TikTok
+                </a>
+                <a href="https://pinterest.com/centralmacatuba" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/pinterest.svg" class="social-icon"> Pinterest
+                </a>
+                <a href="https://t.me/centralmacatuba" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/telegram.svg" class="social-icon"> Telegram
+                </a>
+                <a href="https://linkedin.com/company/centralmacatuba" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/linkedin.svg" class="social-icon"> LinkedIn
+                </a>
+            </div>
+        </div>
+    </div>
+        </div>
+
+        <!-- Coluna Elias Empresas -->
+        <div class="footer-col">
+            <h3>Elias Empresas</h3>
+            <p>© 2025 Elias Empresas. Todos os direitos reservados.</p>
+            <div class="social-media">
+                <a href="https://youtube.com/@eliasempresas" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/youtube.svg" class="social-icon"> YouTube
+                </a>
+                <a href="https://facebook.com/eliasempresas" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/facebook.svg" class="social-icon"> Facebook
+                </a>
+                <a href="https://instagram.com/eliasempresas" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/instagram.svg" class="social-icon"> Instagram
+                </a>
+                <a href="https://x.com/eliasempresas" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/x-twitter.svg" class="social-icon"> X
+                </a>
+                <a href="https://tiktok.com/@eliasempresas" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/tiktok.svg" class="social-icon"> TikTok
+                </a>
+                <a href="https://pinterest.com/eliasempresas" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/pinterest.svg" class="social-icon"> Pinterest
+                </a>
+                <a href="https://t.me/eliasempresas" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/telegram.svg" class="social-icon"> Telegram
+                </a>
+                <a href="https://linkedin.com/company/eliasempresas" target="_blank">
+                    <img src="https://arquivos.eliasempresas.com/ARQUIVOS/IMAGENS/linkedin.svg" class="social-icon"> LinkedIn
+                </a>
+            </div>
+        </div>
+    </div>
+
+<script src="https://assets.eliasempresas.com/scripts/avisos.js"></script>
